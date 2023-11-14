a9e235b0fafb233e7cced3efe96d4ba1afb9fa82
Kebiled <kieran.meekan@gmail.com>
2023-11-15 09:44:53 +1300

    Add word

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-400">
       <Box className="w-[800px] min-h-[400px] bg-slate-200 rounded-md">
-        <Text className="text-black">HOME</Text>
+        <Text className="text-black">HOME BASE</Text>
       </Box>
     </main>
   );
