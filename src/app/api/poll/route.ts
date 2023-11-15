fe518b030f8e19c8a46da226d960d4215b9ca11d
Kebiled <kieran.meekan@gmail.com>
2023-11-15 15:13:47 +1300

    fix static api route

--- src/app/api/poll/route.ts
+++ src/app/api/poll/route.ts
@@ -2,6 +2,8 @@ import { pushObjectToDatabase } from "@/utils/prismaUtil";
 
 const GAME_NUMBER = "6420290023981056";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     // Fetch the game object from the API
@@ -9,7 +11,6 @@ export async function GET() {
       `https://np.ironhelmet.com/api?game_number=6420290023981056&code=gazRh6&api_version=0.1`,
       {
         method: "POST",
-        cache: "no-store",
       }
     );
 
@@ -19,7 +20,7 @@ export async function GET() {
 
     const gameData = await response.json();
 
-    // Push the game object data to the database using your function
+    // // Push the game object data to the database using your function
     await pushObjectToDatabase(gameData, GAME_NUMBER);
 
     return new Response("Game object successfully pushed to the database", {
